59dcbc3d34d54265ba24ff097f0e2b8e5f35a6b2
greko3450 <johannsm03@gmail.com>
2023-11-27 19:55:31 -0500

    actual

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const { conn } = require("./src/db.js");
 const {PORT} = process.env
 // Syncing all the models at once.
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`); // eslint-disable-line no-console
   });
